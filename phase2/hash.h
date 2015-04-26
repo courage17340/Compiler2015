aa891b0bdfcfdb39aafb6315dbc030b501d784f7
courage17340 <courage17340@163.com>
2015-04-26 23:07:21 +0800

    backup

--- phase2/hash.h
+++ phase2/hash.h
@@ -2,43 +2,68 @@
 #define _HASH_H_
 #include <stdlib.h>
 #include <string.h>
-static int SIZE = 109;
-struct node{
-	char *key;
+#define HASH_SIZE 10007
+struct Hash{
+	char *name;
 	void *bind;
-	struct node *next;
+	struct Symbol{
+		char *name;
+		struct Symbol *next;
+	} *sym;
+	struct Hash *next;
 };
-static struct node *table[SIZE];
-static unsigned int hash(char *s0){
-	unsigned int ret = 0;
-	char *s;
-	for (s = s0;*s;++s) ret = ret * 65599 + *s;//natural exceed
+static struct Hash *hashTable[HASH_SIZE];
+static struct Symbol *symTable[HASH_SIZE];
+static int hash(char *s){
+	char *t;
+	int ret = 0;
+	for (t = s;*t;++t)
+		ret = ret * 65599 + *t;
+	return ret % HASH_SIZE;
+}
+static struct Symbol *makeSymbol(char *s,struct Symbol *next){
+	struct Symbol *ret = malloc(sizeof *ret);
+	ret->name = s;
+	ret->next = next;
 	return ret;
 }
-static node *getNode(char *key,void *bind,struct node *next){
-	struct node *b = malloc(sizeof(*b));
-	b->key = key;
-	b->bind = bind;
-	b->next = next;
-	return b;
+static struct Hash *makeHash(char *s,void *bind,struct Symbol *sym,struct Hash *next){
+	struct Hash *ret = malloc(sizeof *ret);
+	ret->name = s;
+	ret->bind = bind;
+	ret->sym = sym;
+	ret->next = next;
+	return ret;
 }
-void insert(char *key,void *bind){
-	int i = hash(key) % SIZE;
-	table[i] = getNode(key,bind,table[i]);
-}
-void *find(char *key){
-	int i = hash(key) % SIZE;
-	struct node *b;
-	for (b = table[i];b != NULL;b = b->next)
-		if (strcmp(b->key,key) == 0) return b->bind;
-	return NULL;
-}
-void pop(char *key){
-	int i = hash(key) % SIZE;
-	struct node *t = table[i];
-	if (t != NULL){
-		table[i] = t->next;
-		free(t);
+static struct Symbol *getSymbol(char *s,int flag){
+	int i = hash(s);
+	struct Symbol *sym;
+	for (sym = symTable[i];sym != NULL;sym = sym->next)
+		if (strcmp(sym->name,s) == 0) return sym;
+	if (flag){
+		symTable[i] = makeSymbol(s,symTable[i]);
+		return symTable[i];
+	}else{
+		return NULL;
 	}
+}
+void pushHash(char *s,void *bind){
+	struct Symbol *sym = getSymbol(s,1);
+	int i = hash(s);
+	hashTable[i] = makeHash(s,bind,sym,hashTable[i]);
+}
+void popHash(char *s){
+	int i = hash(s);
+	struct Hash *tmp = hashTable[i];
+	hashTable[i] = tmp->next;
+	free(tmp);
+}
+void *getHash(char *s){
+	struct Symbol* sym = getSymbol(s,0);
+	int i = hash(s);
+	struct Hash *t;
+	for (t = hashTable[i];t != NULL;t = t->next)
+		if (t->sym == sym) return t->bind;
+	return NULL;
 }
 #endif
